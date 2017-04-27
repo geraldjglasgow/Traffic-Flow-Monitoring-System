92f2723321f4c46d046da626186918948cab36e7
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-04-27 14:54:07 -0400

    Update start_probemon.sh

--- Program/start_probemon.sh
+++ Program/start_probemon.sh
@@ -1,15 +1,10 @@
- #!/bin/bash
-
-echo "Hello World"
+sleep 30
 sudo ifconfig wlan0 down
-echo "wlan0 down"
-sudo ifconfig wlan1 down
-echo "wlan1 down"
-sudo iwconfig wlan1 mode Monitor
-echo "wlan1 Monitor mode"
-sudo ifconfig wlan1 up
-echo "wlan1 up"
-cd ../..
-cd Desktop/probemon-master
-echo "Starting probemon"
-sudo python probemon.py -i wlan1 -f
+sleep 3
+sudo iwconfig wlan0 mode Monitor
+sleep 3
+sudo ifconfig wlan0 up
+sleep 3
+cd /home/pi/path to probemon.py
+sleep 2
+sudo python probemon.py -i wlan0 -r -t unix
