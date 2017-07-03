dfc1098d7efc63fa8547ec362c1cf81b5599e225
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-07-03 08:39:33 -0400

    Update adhoc.sh

--- Program/adhoc.sh
+++ Program/adhoc.sh
@@ -3,10 +3,6 @@ sudo ifconfig wlan0 down
 sleep 2
 sudo ifconfig wlan1 down
 sleep 5
-sudo iwconfig wlan1 mode Monitor
+sudo iwconfig wlan1 channel 1 essid Pi mode ad-hoc
 sleep 3
 sudo ifconfig wlan1 up
-sleep 3
-cd /home/pi/path to probemon.py
-sleep 2
-sudo python probemon.py -i wlan1 -r -t uni
